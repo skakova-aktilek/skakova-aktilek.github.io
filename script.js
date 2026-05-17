ddb5b72d23404c66429d4cd58179433fbedcbe58
Aktilek Skakova <skakova@seas.upenn.edu>
2026-05-17 13:39:11 -0400

    Refactor showTab function to use parameters

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function showTab(tabName) {
-  const tabContents = document.querySelectorAll(".tab-content");
-  const tabButtons = document.querySelectorAll(".tab");
+function showTab(tabId, clickedButton) {
+  const contents = document.querySelectorAll(".tab-content");
+  const buttons = document.querySelectorAll(".tab");
 
-  tabContents.forEach((content) => {
-    content.classList.remove("active");
-  });
+  contents.forEach((content) => content.classList.remove("active"));
+  buttons.forEach((button) => button.classList.remove("active"));
 
-  tabButtons.forEach((button) => {
-    button.classList.remove("active");
-  });
-
-  document.getElementById(tabName).classList.add("active");
-  event.target.classList.add("active");
+  document.getElementById(tabId).classList.add("active");
+  clickedButton.classList.add("active");
 }
